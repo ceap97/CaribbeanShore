4f40e4c70ea8f85c47d3dedf39bad32a90a2e6a7
Cesar Emilio Arboleda Palomeque <cesar_arboleda@soy.sena.edu.co>
2024-11-25 17:40:12 -0500

    1.5

---
+++ RefugioVerde/Models/CatalogoViewModel.cs
@@ -0,0 +1,7 @@
+﻿using RefugioVerde.Models;
+
+public class CatalogoViewModel
+{
+    public IEnumerable<Habitacion> Habitaciones { get; set; }
+    public bool UsuarioAsociadoConCliente { get; set; }
+}
