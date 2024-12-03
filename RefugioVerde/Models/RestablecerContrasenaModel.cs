09f3560447e1723fc58899425cf8a65b8d2256ee
Cesar Emilio Arboleda Palomeque <cesar_arboleda@soy.sena.edu.co>
2024-12-02 20:16:33 -0500

    11.0

---
+++ RefugioVerde/Models/RestablecerContrasenaModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RefugioVerde.Models
+{
+    public class RestablecerContrasenaModel
+    {
+        [Required(ErrorMessage = "El correo es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El correo no tiene un formato válido.")]
+        public string Correo { get; set; }
+
+        [Required(ErrorMessage = "El token es obligatorio.")]
+        public string Token { get; set; }
+
+        [Required(ErrorMessage = "La nueva contraseña es obligatoria.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "La nueva contraseña debe tener entre 6 y 100 caracteres.")]
+        public string NuevaContrasena { get; set; }
+    }
+}
